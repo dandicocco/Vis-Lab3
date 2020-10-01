66bd1a776a6460ae3a34084aad8e15e0e317961e
Daniel DiCocco <dicoccod@bc.edu>
2020-09-30 21:47:08 -0400

    finished bar chart

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ d3.csv('cities.csv', d3.autoType).then(data=>{
     d3.select('.city-count').text(function() {
         return "Number of Cities: " + cityData.length}); //display Number of Cities
 
+    //construct svg element
     const w = 700;
     const h = 550;
     const svg = d3.select('.population-plot')
@@ -59,5 +60,81 @@ d3.csv('cities.csv', d3.autoType).then(data=>{
         
 
 })
+
+let buildingData;
+
+d3.csv('buildings.csv', d3.autoType).then(bData => {
+		buildingData = bData;
+        buildingData.sort((a, b) => b.height_ft - a.height_ft);  //sort desc
+
+        console.log('Buildings', buildingData);
+
+        //construct second SVG element
+        const w = 500;
+        const h = 500;
+        let barPadding =10;
+        const svg = d3.select('.building-plot')
+                        .append('svg')
+                        .attr('width', w)
+                        .attr('height', h)
+
+        //create bars
+        svg.selectAll(".bar")
+            .data(buildingData)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", 200)
+            .attr("y", function(d,i){
+                return i * (h / buildingData.length)
+            })
+            .attr("width", function(d){
+                return d.height_px;
+            })
+            .attr("height", h / buildingData.length - barPadding)
+            .attr("fill", "#f29c38");
+    
+        //add building names
+        svg.selectAll("text.labels")
+            .data(buildingData)
+            .enter()
+            .append("text")
+            .attr("class", "labels")
+            .text(function(d){
+                return d.building;
+            })
+            .attr("x", 0)
+            .attr("y", function(d,i){
+                // the + 1 at the end is an aesthetic adjustment, alignment baseline though middle is not true middle
+                return (i * (h / buildingData.length) + (h / buildingData.length - barPadding) / 2) + 1;
+            })
+            .attr("alignment-baseline", "middle")
+            .attr("font-size", 13)
+            .attr("font-weight", "bolder");
+
+        //add building sizes
+        svg.selectAll("text.sizes")
+            .data(buildingData)
+            .enter()
+            .append("text")
+            .attr("class", "sizes")
+            .text(function(d){
+                return d.height_ft + " ft";
+            })
+            .attr("x", function(d){
+                return 150 + d.height_px;
+            })
+            .attr("y", function(d,i){
+                // the + 1 at the end is an aesthetic adjustment, alignment baseline though middle is not true middle
+                return (i * (h / buildingData.length) + (h / buildingData.length - barPadding) / 2) + 1;
+            })
+            .attr("alignment-baseline", "middle")
+            .attr("font-size", 13)
+            .attr("fill", "white");
+
+
+
+
+    })
 
 
